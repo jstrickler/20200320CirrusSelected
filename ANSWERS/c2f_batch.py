cd8aef109a43dd751133ea62799ea61c4142e23b
jstrickler <jstrickler@gmail.com>
2020-03-30 16:13:07 -0400

    Monday 4:13 PM

--- ANSWERS/c2f_batch.py
+++ ANSWERS/c2f_batch.py
@@ -2,9 +2,11 @@
 
 import sys
 
-celsius = float(sys.argv[1])
+if len(sys.argv[1]) > 1:
+    celsius = float(sys.argv[1])
 
-fahrenheit = ((9 * celsius) / 5) + 32
+    fahrenheit = ((9 * celsius) / 5) + 32
 
-print("{:.1f} C is {:.1f} F".format(celsius, fahrenheit))
-
+    print("{:.1f} C is {:.1f} F".format(celsius, fahrenheit))
+else:
+    print("Please provide a number on the command line")
